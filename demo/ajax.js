a9ae1bba2fae1971a7aebd281de69ecc21f97a24
CangShi <1184518756@qq.com>
2017-09-22 13:10:22 +0800

    新增ajax中的url指定劫持，完成基本功能

---
+++ demo/ajax.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Eoly on 2017/5/8.
+ * Call http://palerock.cn
+ */
+var Ajax = function (conf) {
+    if (!conf || typeof conf != 'object') {
+        throw new Error('配置类型错误');
+    }
+    if (conf.params && typeof conf.params === 'object') {
+        var params = conf.params;
+        for (var name in params) {
+            if (params.hasOwnProperty(name))
+            if (params[name]) {
+                if (typeof params[name] != 'string' ){
+                    continue;
+                }
+                if (conf.url.indexOf('?') != -1) {
+                    conf.url = conf.url + '&' + name + '=' + params[name];
+                }else {
+                    conf.url = conf.url + '?' + name + '=' + params[name];
+                }
+            }
+        }
+    }
+    Ajax.sentRequest(conf);
+    return Ajax;
+};
+Ajax.get = function () {
+};
+Ajax.then = function (suc, fail) {
+    Ajax.OnSuccess = suc;
+    Ajax.OnFailure = fail;
+};
+Ajax.OnFailure = function (result) {
+};
+Ajax.OnSuccess = function (result) {
+};
+Ajax.onReady = function (e) {
+    var element = e.srcElement;
+    var result = {};
+    if (element.status >= 200 && element.status < 400) {
+        try {
+            result.data = JSON.parse(element.responseText);
+        } catch (ex) {
+            result.data = element.responseText;
+        }
+        this.OnSuccess(result.data);
+    } else if (element.status >= 400) {
+        this.OnFailure(result);
+    }
+};
+Ajax.sentRequest = function (conf) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(conf.method, conf.url, conf.async);
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4) {
+            Ajax.onReady(e)
+        }
+    };
+    xhr.send();
+};
+// Ajax({
+//     url: 'http://palerock.cn/h5_back/group/find-groups',
+//     method: 'get',
+//     params: {goodsTypeId: 11},
+//     async: true
+// });
--- src/everything-hook.js
+++ src/everything-hook.js
@@ -5,7 +5,7 @@
  * View: http://palerock.cn
  * ---------------------------
  */
-// 'use strict';
+'use strict';
 ~function (utils) {
     var _global = this;
     var EHook = function () {
@@ -75,12 +75,11 @@
         /**
          * 执行多个方法并注入一个方法和参数集合
          * @param context
-         * @param method
          * @param methods
          * @param args
          * @private
          */
-        _invokeMethods: function (context, method, methods, args) {
+        _invokeMethods: function (context, methods, args) {
             if (!utils.isArray(methods)) {
                 return;
             }
@@ -88,23 +87,27 @@
                 if (!utils.isFunction(_method.method)) {
                     return;
                 }
-                _method.method.call(this, method, args);
+                _method.method.apply(this, args);
             });
         },
         /**
          * 生成和替换劫持方法
+         * @param parent
          * @param context
          * @param methodName {string}
          * @private
          */
-        _hook: function (context, methodName) {
-            var method = context[methodName];
-            var methodTask = this._getHookedMethodTask(context, methodName);
+        _hook: function (parent, methodName, context) {
+            if (context === undefined) {
+                context = parent
+            }
+            var method = parent[methodName];
+            var methodTask = this._getHookedMethodTask(parent, methodName);
             if (!methodTask.original) {
                 methodTask.original = method;
             }
             if (utils.isExistObject(methodTask.replace) && utils.isFunction(methodTask.replace.method)) {
-                context[methodName] = methodTask.replace.method(methodTask.original);
+                parent[methodName] = methodTask.replace.method(methodTask.original);
                 return;
             }
             var invokeMethods = this._invokeMethods;
@@ -112,32 +115,35 @@
             var methodStr = '(function(){\n';
             methodStr = methodStr + 'var result = undefined;\n';
             if (methodTask.task.before.length > 0) {
-                methodStr = methodStr + 'invokeMethods(context, methodTask.original, methodTask.task.before, arguments);\n';
+                methodStr = methodStr + 'invokeMethods(context, methodTask.task.before,[methodTask.original, arguments]);\n';
             }
             if (utils.isFunction(methodTask.task.current)) {
-                methodStr = methodStr + 'result = methodTask.task.current.method.call(context, context, methodTask.original, arguments);\n';
+                methodStr = methodStr + 'result = methodTask.task.current.method.call(context, parent, methodTask.original, arguments);\n';
             } else {
                 methodStr = methodStr + 'result = methodTask.original.apply(context, arguments);\n';
             }
             if (methodTask.task.after.length > 0) {
-                methodStr = methodStr + 'invokeMethods(context, methodTask.original, methodTask.task.after, arguments);\n';
+                methodStr = methodStr + 'var args = [];args.push(methodTask.original);args.push(arguments);args.push(result);\n';
+                methodStr = methodStr + 'invokeMethods(context, methodTask.task.after, args);\n';
             }
             methodStr = methodStr + 'return result;\n})';
             // 绑定劫持函数
-            context[methodName] = eval(methodStr);
+            parent[methodName] = eval(methodStr);
         },
         /**
          * 劫持一个方法
-         * @param context
+         * @param parent
          * @param methodName {string}
          * @param config
          */
-        hook: function (context, methodName, config) {
+        hook: function (parent, methodName, config) {
             var hookedFailure = -1;
-            if (!context[methodName] || !utils.isFunction(context[methodName])) {
+            // 调用方法的上下文
+            var context = config.context !== undefined ? config.context : parent;
+            if (!parent[methodName] || !utils.isFunction(parent[methodName])) {
                 return hookedFailure;
             }
-            var methodTask = this._getHookedMethodTask(context, methodName);
+            var methodTask = this._getHookedMethodTask(parent, methodName);
             var id = this._getAutoId();
             if (utils.isFunction(config.replace)) {
                 methodTask.replace = {
@@ -168,7 +174,7 @@
                 hookedFailure = 0;
             }
             if (hookedFailure === 0) {
-                this._hook(context, methodName);
+                this._hook(parent, methodName, context);
                 return id;
             } else {
                 return hookedFailure;
@@ -177,29 +183,34 @@
         },
         /**
          * 劫持替换一个方法
+         * @param parent
          * @param context
          * @param methodName {string}
          * @param replace {function}
          * @return {number} 该次劫持的id
          */
-        hookReplace: function (context, methodName, replace) {
-            return this.hook(context, methodName, {
-                replace: replace
+        hookReplace: function (parent, methodName, replace, context) {
+            return this.hook(parent, methodName, {
+                replace: replace,
+                context: context
             })
         },
-        hookBefore: function (context, methodName, before) {
-            return this.hook(context, methodName, {
-                before: before
+        hookBefore: function (parent, methodName, before, context) {
+            return this.hook(parent, methodName, {
+                before: before,
+                context: context
             })
         },
-        hookCurrent: function (context, methodName, current) {
-            return this.hook(context, methodName, {
-                current: current
+        hookCurrent: function (parent, methodName, current, context) {
+            return this.hook(parent, methodName, {
+                current: current,
+                context: context
             })
         },
-        hookAfter: function (context, methodName, after) {
-            return this.hook(context, methodName, {
-                after: after
+        hookAfter: function (parent, methodName, after, context) {
+            return this.hook(parent, methodName, {
+                after: after,
+                context: context
             })
         },
         /**
@@ -208,14 +219,14 @@
          * @return {*|number} 劫持的id
          */
         hookAjax: function (methods) {
+            var _this = this;
             var hookMethod = function (methodName) {
-                return function () {
-                    var args = [].slice.call(arguments);
-                    if (methods[methodName] && methods[methodName].call(this, args, this.xhr)) {
-                        return;
-                    }
-                    return this.xhr[methodName].apply(this.xhr, args);
-                };
+                if (utils.isFunction(methods[methodName])) {
+                    // 在执行方法之前hook原方法，除开on开头的方法
+                    _this.hookBefore(this.xhr, methodName, methods[methodName]);
+                }
+                // 返回方法调用内部的xhr
+                return this.xhr[methodName].bind(this.xhr);
             };
             var getFactory = function (attr) {
                 return function () {
@@ -225,15 +236,16 @@
             var setFactory = function (attr) {
                 return function (f) {
                     var xhr = this.xhr;
-                    var that = this;
                     if (attr.indexOf("on") !== 0) {
                         this[attr + "_"] = f;
                         return;
                     }
                     if (methods[attr]) {
                         xhr[attr] = function () {
-                            methods[attr](that) || f.apply(xhr, arguments);
+                            f.apply(xhr, arguments);
                         };
+                        // on方法在set时劫持
+                        _this.hookBefore(xhr, attr, methods[attr]);
                     } else {
                         xhr[attr] = f;
                     }
@@ -245,7 +257,8 @@
                     for (var propertyName in this.xhr) {
                         var property = this.xhr[propertyName];
                         if (utils.isFunction(property)) {
-                            this[propertyName] = hookMethod(propertyName);
+                            // hook 原方法
+                            this[propertyName] = hookMethod.bind(this)(propertyName);
                         } else {
                             Object.defineProperty(this, propertyName, {
                                 get: getFactory(propertyName),
@@ -274,10 +287,143 @@
             }
         }
     };
+    var eHook = new EHook();
     var AHook = function () {
-        this.urlDispatcherList = [];
+        this.isHooked = false;
+        var autoId = 1;
+        this._urlDispatcherList = [];
+        this._getAutoId = function () {
+            return autoId++;
+        }
     };
-    _global['eHook'] = new EHook();
+    AHook.prototype = {
+        /**
+         * 执行配置列表中的指定方法组
+         * @param xhr
+         * @param methodName
+         * @param args
+         * @private
+         */
+        _invokeAimMethods: function (xhr, methodName, args) {
+            var configs = utils.parseArrayByProperty(xhr.patcherList, 'config');
+            var methods = [];
+            utils.ergodicArrayObject(xhr, configs, function (config) {
+                if (utils.isFunction(config[methodName])) {
+                    methods.push(config[methodName]);
+                }
+            });
+            utils.invokeMethods(xhr, methods, args);
+        },
+        /**
+         * 根据url获取配置列表
+         * @param url
+         * @return {Array}
+         * @private
+         */
+        _urlPatcher: function (url) {
+            var patcherList = [];
+            utils.ergodicArrayObject(this, this._urlDispatcherList, function (patcherMap, i) {
+                if (utils.urlUtils.urlMatching(url, patcherMap.patcher)) {
+                    patcherList.push(patcherMap);
+                }
+            });
+            return patcherList;
+        },
+        /**
+         * 根据xhr对象分发回调请求
+         * @param xhr
+         * @param fullUrl
+         * @private
+         */
+        _xhrDispatcher: function (xhr, fullUrl) {
+            var url = utils.urlUtils.getUrlWithoutParam(fullUrl);
+            xhr.patcherList = this._urlPatcher(url);
+        },
+        /**
+         * 解析open方法的参数
+         * @param args
+         * @private
+         */
+        _parseOpenArgs: function (args) {
+            return {
+                method: args[0],
+                fullUrl: args[1],
+                url: utils.urlUtils.getUrlWithoutParam(args[1]),
+                params: utils.urlUtils.getParamFromUrl(args[1]),
+                async: args[2]
+            };
+        },
+        _rebuildOpenArgs: function (argsObject, argsArray) {
+            argsArray[0] = argsObject.method;
+            argsArray[1] = utils.urlUtils.margeUrlAndParams(argsObject.url, argsObject.params);
+            argsArray[2] = argsObject.async;
+        },
+        /**
+         * 手动开始劫持
+         */
+        startHook: function () {
+            var _this = this;
+            var _getHookedArgs = function (args) {
+                // 获取劫持方法的参数 [原方法,原方法参数,原方法返回值]
+                // 将参数中'原方法'剔除
+                return Array.prototype.slice.call(args, 0).splice(1);
+            };
+            var normalMethods = {
+                // 拦截响应
+                onreadystatechange: function () {
+                    if (this.readyState == 4 && this.status == 200 || this.status == 304) {
+                        var args = _getHookedArgs(arguments);
+                        _this._invokeAimMethods(this, 'onreadystatechange', args);
+                    }
+                },
+                onload: function () {
+                    var args = _getHookedArgs(arguments);
+                    _this._invokeAimMethods(this, 'onload', args);
+                },
+                // 拦截请求
+                open: function () {
+                    var args = _getHookedArgs(arguments);
+                    var fullUrl = args[0][1];
+                    _this._xhrDispatcher(this, fullUrl);
+                    var argsObject = _this._parseOpenArgs(args[0]);
+                    _this._invokeAimMethods(this, 'open', [argsObject]);
+                    _this._rebuildOpenArgs(argsObject, args[0]);
+                }
+            };
+            // todo reinfecta [aop切入实现劫持所有参数并赋值]
+            // 设置总的hookId
+            this.___hookedId = _global.eHook.hookAjax(normalMethods);
+            this.isHooked = true;
+        },
+        /**
+         * 注册ajaxUrl拦截
+         * @param urlPatcher
+         * @param config
+         * @return {number}
+         */
+        register: function (urlPatcher, config) {
+            if (!urlPatcher || !utils.isExistObject(config)) {
+                return -1;
+            }
+            var id = this._getAutoId();
+            this._urlDispatcherList.push({
+                // 指定id便于后续取消
+                id: id,
+                patcher: urlPatcher,
+                config: config
+            });
+            // 当注册一个register时，自动开始运行劫持
+            if (!this.isHooked) {
+                this.startHook();
+            }
+            return id;
+        }
+        // todo 注销  cancellation: function (registerId){};
+    };
+
+    _global['eHook'] = eHook;
+    _global['aHook'] = new AHook();
+
 }.bind(window)({
     /**
      * 对象是否为数组
@@ -320,6 +466,116 @@
             if (result && result == -1) {
                 break;
             }
+        }
+    },
+    /**
+     * 遍历对象属性
+     * @param context {object} 上下文
+     * @param obj {object} 遍历对象
+     * @param cb {function} 回调函数
+     * @param isReadInnerObject {boolean=} 是否遍历内部对象的属性
+     */
+    ergodicObject: function (context, obj, cb, isReadInnerObject) {
+        var keys = Object.keys(obj);
+        this.ergodicArrayObject(this, keys, function (propertyName) {
+            // 若内部对象需要遍历
+            var _propertyName = propertyName;
+            if (isReadInnerObject && obj[propertyName] != null && typeof obj[propertyName] == 'object') {
+                this.ergodicObject(this, obj[propertyName], function (value, key) {
+                    return cb.call(context, value, _propertyName + '.' + key);
+                }, true)
+            } else {
+                return cb.call(context, obj[propertyName], propertyName);
+            }
+        })
+    },
+    /**
+     * 获取数组对象的一个属性发起动作
+     * @param context {Object}
+     * @param arr {Array}
+     * @param propertyName {String}
+     * @param cb {Function}
+     * @param checkProperty {boolean} 是否排除不拥有该属性的对象[default:true]
+     */
+    getPropertyDo: function (context, arr, propertyName, cb, checkProperty) {
+        if (checkProperty == null) {
+            checkProperty = true;
+        }
+        this.ergodicArrayObject(context, arr, function (ele) {
+            if (!checkProperty || ele.hasOwnProperty(propertyName)) {
+                cb.call(context, ele[propertyName], ele);
+            }
+        })
+    },
+    /**
+     * 通过数组中每个对象的指定属性生成一个新数组
+     * @param arr {Array}
+     * @param propertyName {String}
+     */
+    parseArrayByProperty: function (arr, propertyName) {
+        var result = [];
+        if (!this.isArray(arr)) {
+            return result;
+        }
+        this.getPropertyDo(this, arr, propertyName, function (value) {
+            result.push(value);
+        }, true);
+        return result;
+    },
+    invokeMethods: function (context, methods, args) {
+        if (!this.isArray(methods)) {
+            return;
+        }
+        var results = [];
+        var _this = this;
+        this.ergodicArrayObject(context, methods, function (method) {
+            if (!_this.isFunction(method)) {
+                return;
+            }
+            results.push(
+                method.apply(_this, args)
+            );
+        });
+        return results;
+    },
+    urlUtils: {
+        urlMatching: function (url, matchUrl) {
+            var pattern = eval('/' + matchUrl.replace(/\//g, '\\/') + '/');
+            return pattern.test(url);
+        },
+        getUrlWithoutParam: function (url) {
+            return url.split('?')[0];
+        },
+        getParamFromUrl: function (url) {
+            var params = [];
+            var parts = url.split('?');
+            if (parts.length < 2) {
+                return params;
+            }
+            var paramsStr = parts[1].split('&');
+            for (var i = 0; i < paramsStr.length; i++) {
+                var ps = paramsStr[i].split('=');
+                if (ps.length < 2) {
+                    continue;
+                }
+                params.push({
+                    key: ps[0],
+                    value: ps[1]
+                });
+            }
+            return params;
+        },
+        margeUrlAndParams: function (url, params) {
+            if (url.indexOf('?') != -1 || !(params instanceof Array)) {
+                return url;
+            }
+            var paramsStr = [];
+            for (var i = 0; i < params.length; i++) {
+                if (params[i].key != null && params[i].value != null) {
+                    paramsStr.push(params[i].key + '=' + params[i].value);
+                }
+            }
+            return url + '?' + paramsStr.join('&');
         }
     }
 });
