711d5c108d821dd3a0bcd851625419f272ff99ec
ryan <1184518756@qq.com>
2020-03-19 16:53:23 +0800

    update

--- src/plugins/timeHooker.js
+++ src/plugins/timeHooker.js
@@ -4,7 +4,7 @@
 // @name:zh-CN   计时器掌控者|视频广告跳过|视频广告加速器
 // @namespace    https://gitee.com/HGJing/everthing-hook/
 // @updateURL    https://gitee.com/HGJing/everthing-hook/raw/master/src/plugins/timeHooker.js
-// @version      1.0.30
+// @version      1.0.31
 // @description       控制网页计时器速度|加速跳过页面计时广告|视频快进（慢放）|跳过广告|支持几乎所有网页.
 // @description:en  it can hook the timer speed to change.
 // @description:zh-CN  控制网页计时器速度|加速跳过页面计时广告|跳过广告|支持几乎所有网页.
@@ -377,13 +377,12 @@ document.addEventListener('readystatechange', function () {
 
                     if (!normalUtil.isInIframe()) {
                         h.applyUI();
+                        h.applyGlobalAction();
                     } else {
                         normalUtil.listenParentEvent((function (percentage) {
                             this.change(percentage);
                         }).bind(this))
                     }
-
-                    h.applyGlobalAction();
 
                     h.registerShortcutKeys();
                 },
