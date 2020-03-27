3b6c1b5a99ab523df9da1f0e9d007f75526391bc
ryan <1184518756@qq.com>
2020-03-27 13:51:32 +0800

    update to fixed an issue

--- src/plugins/timeHooker.js
+++ src/plugins/timeHooker.js
@@ -4,12 +4,12 @@
 // @name:zh-CN   计时器掌控者|视频广告跳过|视频广告加速器
 // @namespace    https://gitee.com/HGJing/everthing-hook/
 // @updateURL    https://gitee.com/HGJing/everthing-hook/raw/master/src/plugins/timeHooker.js
-// @version      1.0.31
+// @version      1.0.35
 // @description       控制网页计时器速度|加速跳过页面计时广告|视频快进（慢放）|跳过广告|支持几乎所有网页.
 // @description:en  it can hook the timer speed to change.
 // @description:zh-CN  控制网页计时器速度|加速跳过页面计时广告|跳过广告|支持几乎所有网页.
 // @include      *
-// @require      https://greasyfork.org/scripts/372672-everything-hook/code/Everything-Hook.js?version=659315
+// @require      https://greasyfork.org/scripts/372672-everything-hook/code/Everything-Hook.js?version=784956
 // @author       Cangshi
 // @match        http://*/*
 // @run-at       document-start
@@ -171,13 +171,16 @@ document.addEventListener('readystatechange', function () {
                 };
                 eHookContext.hookedToString(timerContext._Date.now, Date.now);
                 var objToString = Object.prototype.toString;
-                Object.prototype.toString = function () {
+
+                eHookContext.hookAfter(Object.prototype, 'toString',function (m, args, result) {
                     if (this instanceof timerContext._mDate) {
                         return '[object Date]';
                     } else {
-                        return objToString.call(this);
+                        return result;
                     }
-                };
+                }, false);
+
+                eHookContext.hookedToString(objToString, Object.prototype.toString);
                 eHookContext.hookedToString(timerContext._setInterval, setInterval);
                 eHookContext.hookedToString(timerContext._setTimeout, setTimeout);
                 eHookContext.hookedToString(timerContext._clearInterval, clearInterval);
@@ -195,18 +198,66 @@ document.addEventListener('readystatechange', function () {
                         });
                         return;
                     } else if (arguments.length > 1) {
+                        var definedValue;
+                        switch (arguments.length) {
+                            case 2:
+                                definedValue = new timerContext._Date(
+                                    arguments[0],
+                                    arguments[1]
+                                );
+                                break;
+                            case 3:
+                                definedValue = new timerContext._Date(
+                                    arguments[0],
+                                    arguments[1],
+                                    arguments[2],
+                                );
+                                break;
+                            case 4:
+                                definedValue = new timerContext._Date(
+                                    arguments[0],
+                                    arguments[1],
+                                    arguments[2],
+                                    arguments[3],
+                                );
+                                break;
+                            case 5:
+                                definedValue = new timerContext._Date(
+                                    arguments[0],
+                                    arguments[1],
+                                    arguments[2],
+                                    arguments[3],
+                                    arguments[4]
+                                );
+                                break;
+                            case 6:
+                                definedValue = new timerContext._Date(
+                                    arguments[0],
+                                    arguments[1],
+                                    arguments[2],
+                                    arguments[3],
+                                    arguments[4],
+                                    arguments[5]
+                                );
+                                break;
+                            default:
+                            case 7:
+                                definedValue = new timerContext._Date(
+                                    arguments[0],
+                                    arguments[1],
+                                    arguments[2],
+                                    arguments[3],
+                                    arguments[4],
+                                    arguments[5],
+                                    arguments[6]
+                                );
+                                break;
+                        }
+
                         Object.defineProperty(this, '_innerDate', {
                             configurable: false,
                             enumerable: false,
-                            value: new timerContext._Date(
-                                arguments[0] || null,
-                                arguments[1] || null,
-                                arguments[2] || null,
-                                arguments[3] || null,
-                                arguments[4] || null,
-                                arguments[5] || null,
-                                arguments[6] || null
-                            ),
+                            value: definedValue,
                             writable: false
                         });
                         return;
