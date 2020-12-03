e8086901cce4322d2f7b7c690bfa29e5ea5103f2
ryan <1184518756@qq.com>
2020-12-03 18:09:47 +0800

    copy lifted 0.2

--- src/plugins/copying-lifted/copyingLifted.js
+++ src/plugins/copying-lifted/copyingLifted.js
@@ -3,7 +3,7 @@
 // @name:en      Copying Lifted
 // @name:zh-CN   Copying Lifted 解除复制限制
 // @namespace    https://palerock.cn
-// @version      0.1
+// @version      0.2
 // @description:en  Allow your copying in anywhere.
 // @description:zh-CN  让你在几乎所有网页里自由地复制粘贴，而不用担心被限制，或者被篡改复制内容
 // @require      https://greasyfork.org/scripts/372672-everything-hook/code/Everything-Hook.js?version=659315
@@ -14,7 +14,12 @@
 // @grant        none
 // ==/UserScript==
 
-var preventDomList = ['window', 'document'];
+function removeCopyEvent() {
+    document.querySelectorAll('*')
+        .forEach(function (ele) {
+            ele.oncopy = undefined;
+        });
+}
 
 'use strict';
 ~function (global) {
@@ -28,12 +33,14 @@ var preventDomList = ['window', 'document'];
                     }
                 }, false);
 
-            preventDomList.forEach(function (dom) {
-                Object.defineProperty(global[dom], 'oncopy', {
-                    get: function () {
-                        return undefined;
-                    }
-                });
+            global.addEventListener('keydown', function (e) {
+                if (e.ctrlKey || e.keyCode === 224 || e.keyCode === 17 || e.keyCode === 91 || e.keyCode === 93) {
+                    removeCopyEvent();
+                }
+            });
+
+            global.addEventListener('contextmenu', function (e) {
+                removeCopyEvent();
             });
 
             console.log('[AllowCopy Plugins]', 'works.');
